9c541efbe8393ad5ec633488fb809d1bb04db6f6
Dawid Krystochowicz <daw_kryst@interia.pl>
2020-06-14 18:46:00 +0200

    add file upload to contact form

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/uploads/')
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  }
+})
+const upload = multer({ storage: storage});
 
 const app = express();
 app.engine('hbs', hbs({ extname: 'hbs', defaultLayout: 'main' }));
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/user',(req, res) => {
   res.send('You have to be logged to see this page!');
@@ -26,6 +37,18 @@ app.get('/about', (req, res) => {
 
 app.get('/contact', (req, res) => {
   res.render('contact');
+});
+
+app.post('/contact/send-message', upload.single('file'), async (req, res) => {
+  const { author, sender, title, message } = req.body;
+  const file = req.file;
+
+  if(author && sender && title && message && file) {
+    res.render('contact', { isSent: true, file: file.originalname });
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
 });
 
 app.get('/info', (req, res) => {
